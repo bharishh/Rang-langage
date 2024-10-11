19a790a3035938e6459e4b3699f79d7e49c8de79
bharishh <harish_01@icloud.com>
2024-10-11 19:28:40 +0200

    affichage du tableau

--- index.php
+++ index.php
@@ -12,7 +12,10 @@ foreach($top10 as $ttop10): ?>
 <ul>
 
     <pre>
-        <?= "<li>".htmlspecialchars($ttop10['id_rang'])." </li>".htmlspecialchars($ttop10['langage'])." ".htmlspecialchars($ttop10['description'])." ".htmlspecialchars($ttop10['utilisationPrincipal'])."<br>"
+        <?= "<li>".htmlspecialchars($ttop10['id_rang'])." </li>"
+        .htmlspecialchars($ttop10['langage'])." ".
+        htmlspecialchars($ttop10['description'])." ".
+        htmlspecialchars($ttop10['utilisationPrincipal'])
     ?>
     </pre>
     
